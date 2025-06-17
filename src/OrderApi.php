634c1d816bdbbf88041ef541a9595df20c35d4a8
dorgan@donaldorgan.com <dorgan@donaldorgan.com>
2025-06-17 18:40:52 -0400

    Update OrderApi.php

--- src/OrderApi.php
+++ src/OrderApi.php
@@ -51,7 +51,11 @@ class OrderApi {
         }
 
         if ($this->statusCode != 200 && $this->statusCode != 202) {
-            throw new \Exception('Error: ' . $this->response->getStatusCode());
+            if (isset($this->response) && !is_null($this->response) && method_exists($this->response, 'getStatusCode')) {
+                throw new \Exception('Error: ' . $this->response->getStatusCode());
+            } else {
+                throw new \Exception('Error: ' . $this->statusCode);
+            }
         }
 
         try {
