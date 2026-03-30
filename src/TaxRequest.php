13fe7598e1801e448446e49a9c5bbc2dc1968654
Donald Organ <dorgan@donaldorgan.com>
2026-03-30 18:44:38 -0400

    Update PHP version requirement to >=8.2 and enhance Address class to validate country input

--- src/TaxRequest.php
+++ src/TaxRequest.php
@@ -1,254 +1,260 @@
 <?php
-    namespace AccurateTax;
+namespace AccurateTax;
 
-    use \AccurateTax\TaxRequest\Order;
-    use GuzzleHttp\Client;
+use \AccurateTax\TaxRequest\Order;
+use GuzzleHttp\Client;
 
-    class TaxRequest {
-        /**
-         * @var string The License Key to use for the request
-         */
-        private string $licensekey;
+class TaxRequest
+{
+    /**
+     * @var string The License Key to use for the request
+     */
+    private string $licensekey;
 
-        /**
-         * @var string The Checksum to use for the request
-         */
-        private string $checksum;
+    /**
+     * @var string The Checksum to use for the request
+     */
+    private string $checksum;
 
-        /**
-         * @var Order The Order to send to AccurateTax
-         */
-        private TaxRequest\Order $order;
+    /**
+     * @var Order The Order to send to AccurateTax
+     */
+    private TaxRequest\Order $order;
 
-        /**
-         * @var bool Flag to commit the order
-         */
-        private bool $commit = false;
+    /**
+     * @var bool Flag to commit the order
+     */
+    private bool $commit = false;
 
-        /**
-         * @var string The domain to send the request to
-         */
-        private string $domain = 'us1.accuratetax.com';
+    /**
+     * @var string The domain to send the request to
+     */
+    private string $domain = 'us1.accuratetax.com';
 
-        /**
-         * @var string The end point to send the request to
-         */
-        private string $endPoint = '/service.php';
+    /**
+     * @var string The end point to send the request to
+     */
+    private string $endPoint = '/service.php';
 
-        /**
-         * @var \SimpleXMLElement The response from the request
-         */
-        public $response;
+    /**
+     * @var \SimpleXMLElement The response from the request
+     */
+    public $response;
 
-        /**
-         * @var array Errors encountered during the request
-         */
-        public array $errors = [];
+    /**
+     * @var array Errors encountered during the request
+     */
+    public array $errors = [];
 
-        /**
-         * @var array meta data for the request
-         */
-        public array $meta = [];
+    /**
+     * @var array meta data for the request
+     */
+    public array $meta = [];
 
-        /**
-         * @var string Source
-         */
-        private string $source;
+    /**
+     * @var string Source
+     */
+    private string $source;
 
-        /**
-         * Create a TaxRequest
-         * @param string $licensekey
-         * @param string $checksum
-         */
-        public function __construct(string $licensekey, string $checksum)
-        {
-            $this->licensekey = $licensekey;
-            $this->checksum = $checksum;
+    /**
+     * Create a TaxRequest
+     * @param string $licensekey
+     * @param string $checksum
+     */
+    public function __construct(string $licensekey, string $checksum)
+    {
+        $this->licensekey = $licensekey;
+        $this->checksum   = $checksum;
+    }
+
+    /**
+     * Get the State of the Ship To Address
+     *
+     * @return string
+     */
+    public function setSource(string $source): void
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * Get the domain
+     *
+     * @return string
+     */
+    public function getDomain(): string
+    {
+        return $this->domain;
+    }
+
+    /**
+     * Get Order for Tax Request
+     *
+     * @return \AccurateTax\TaxRequest\Order
+     */
+    public function getOrder(): Order
+    {
+        return $this->order;
+    }
+
+    /**
+     * Set Domain for Request
+     *
+     * @param string $domain
+     * @return void
+     */
+    public function setDomain(string $domain): void
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * Set end point for Request
+     *
+     * @param string $endPoint
+     * @return void
+     */
+    public function setEndPoint(string $endPoint): void
+    {
+        $this->endPoint = $endPoint;
+    }
+
+    /**
+     * Set Commit flag for Request
+     *
+     * @param bool $commit
+     * @return void
+     */
+    public function setCommit(bool $commit): void
+    {
+        $this->commit = $commit;
+    }
+
+    /**
+     * Add an Order to the Tax Request
+     *
+     * @param Order $order
+     * @return void
+     */
+    public function addOrder(Order $order): void
+    {
+        if (isset($this->order)) {
+            throw new \Exception('Order Already assigned');
         }
+        $this->order = $order;
+    }
 
-        /**
-         * Get the State of the Ship To Address
-         *
-         * @return string
-         */
-        public function setSource(string $source): void {
-            $this->source = $source;
+    /**
+     * get Errors from Tax Request
+     *
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Get XML for Tax Request
+     *
+     * @return string
+     */
+    public function getXml(): string
+    {
+        $xml  = '<taxrequest>';
+        $xml .= '<auth>';
+        $xml .= '<licensekey>' . $this->licensekey . '</licensekey>';
+        $xml .= '<checksum>' . $this->checksum . '</checksum>';
+        $xml .= '</auth>';
+        $xml .= '<breakdown>1</breakdown>';
+        $xml .= '<item_breakdown>1</item_breakdown>';
+        $xml .= '<commit>' . ($this->commit ? 'Y' : 'N') . '</commit>';
+        if (isset($this->source) && ! empty($this->source)) {
+            $xml .= '<source>' . $this->source . '</source>';
         }
+        $xml .= $this->order->getXml();
+        $xml .= '</taxrequest>';
 
-        /**
-         * Get the domain
-         *
-         * @return string
-         */
-        public function getDomain(): string
-        {
-            return $this->domain;
-        }
+        return $xml;
+    }
 
-        /**
-         * Get Order for Tax Request
-         *
-         * @return \AccurateTax\TaxRequest\Order
-         */
-        public function getOrder(): Order
-        {
-            return $this->order;
-        }
-
-        /**
-         * Set Domain for Request
-         *
-         * @param string $domain
-         * @return void
-         */
-        public function setDomain(string $domain): void
-        {
-            $this->domain = $domain;
-        }
-
-        /**
-         * Set end point for Request
-         *
-         * @param string $endPoint
-         * @return void
-         */
-        public function setEndPoint(string $endPoint): void
-        {
-            $this->endPoint = $endPoint;
-        }
-
-        /**
-         * Set Commit flag for Request
-         *
-         * @param bool $commit
-         * @return void
-         */
-        public function setCommit(bool $commit): void
-        {
-            $this->commit = $commit;
-        }
-
-        /**
-         * Add an Order to the Tax Request
-         *
-         * @param Order $order
-         * @return void
-         */
-        public function addOrder(Order $order): void
-        {
-            if (isset($this->order)) {
-                throw New \Exception('Order Already assigned');
-            }
-            $this->order = $order;
-        }
-
-        /**
-         * get Errors from Tax Request
-         *
-         * @return array
-         */
-        public function getErrors(): array
-        {
-            return $this->errors;
-        }
-
-        /**
-         * Get XML for Tax Request
-         *
-         * @return string
-         */
-        public function getXml(): string {
-            $xml = '<taxrequest>';
-            $xml .= '<auth>';
-            $xml .= '<licensekey>' . $this->licensekey . '</licensekey>';
-            $xml .= '<checksum>' . $this->checksum . '</checksum>';
-            $xml .= '</auth>';
-            $xml .= '<breakdown>1</breakdown>';
-            $xml .= '<item_breakdown>1</item_breakdown>';
-            $xml .= '<commit>' . ($this->commit ? 'Y' : 'N') . '</commit>';
-            if (isset($this->source) && !empty($this->source)) {
-                $xml .= '<source>' . $this->source .'</source>';
-            }
-            $xml .= $this->order->getXml();
-            $xml .= '</taxrequest>';
-
-            return $xml;
-        }
-
-        /**
-         * Send Tax Request to AccuratTax
-         *
-         * @param bool $returnResponse
-         *
-         * @return array|bool
-         */
-        public function send(bool $returnResponse = true): array|bool {
-            $client = new Client(
-                [
-                    'timeout' => 60
-                ]
-            );
-            $response = $client->request('GET', 'https://' . $this->domain . '/' . $this->endPoint, [
-                'config' => [
-                    'curl' => [
-                        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-                        CURLOPT_USERPWD => $this->licensekey . ':' . $this->checksum
-                    ]
+    /**
+     * Send Tax Request to AccuratTax
+     *
+     * @param bool $returnResponse
+     *
+     * @return array|bool
+     */
+    public function send(bool $returnResponse = true): array|bool
+    {
+        $client   = new Client(
+            [
+                'timeout' => 60,
+            ]
+        );
+        $response = $client->request('GET', 'https://' . $this->domain . '/' . $this->endPoint, [
+            'config' => [
+                'curl' => [
+                    CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                    CURLOPT_USERPWD => $this->licensekey . ':' . $this->checksum
                 ],
-                'form_params' => [
-                    'data' => $this->getXml()
-                ]
-            ]);
+            ],
+            'form_params' => [
+                'data' => $this->getXml(),
+            ],
+        ]);
 
-            try {
-                $prevErrorState = libxml_use_internal_errors(true);;
-                $taxResponse = simplexml_load_string(trim($response->getBody()));
-                $errors = libxml_get_errors();
-                if ( count( $errors ) > 0 ) {
-                    foreach( $errors as $error ) {
-                        array_push($this->errors, $error->message);
-                    }
-                    libxml_clear_errors();
-                    libxml_use_internal_errors($prevErrorState);
-                    if (!$returnResponse) {
-                        return false;
-                    } else {
-                        return array(
-                            'response' => $taxResponse,
-                            'errors' => $this->errors
-                        );
-                    }
-                } else {
-                    $this->response = $taxResponse;
+        try {
+            $prevErrorState = libxml_use_internal_errors(true);
+            ;
+            $taxResponse = simplexml_load_string(trim($response->getBody()));
+            $errors      = libxml_get_errors();
+            if (count($errors) > 0) {
+                foreach ($errors as $error) {
+                    array_push($this->errors, $error->message);
                 }
-
-                if (isset($taxResponse->errors)) {
-                    foreach($taxResponse->errors->error as $err) {
-                        array_push($this->errors, (string)$err);
-                    }
-                }
-
-                if (!$returnResponse) {
-                    if (count($this->errors) > 0) {
-                        return false;
-                    } else {
-                        return true;
-                    }
-                } else {
-                    return array(
-                        'response' => $taxResponse,
-                        'errors' => $this->errors
-                    );
-                }
-            } catch( \Exception $e ){
-                array_push($this->errors, $e->getMessage());
-                if (!$returnResponse) {
+                libxml_clear_errors();
+                libxml_use_internal_errors($prevErrorState);
+                if (! $returnResponse) {
                     return false;
                 } else {
                     return array(
-                        'errors' => $this->errors
+                        'response' => $taxResponse,
+                        'errors' => $this->errors,
                     );
                 }
+            } else {
+                $this->response = $taxResponse;
+            }
+
+            if (isset($taxResponse->errors)) {
+                foreach ($taxResponse->errors->error as $err) {
+                    array_push($this->errors, (string) $err);
+                }
+            }
+
+            if (! $returnResponse) {
+                if (count($this->errors) > 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return array(
+                    'response' => $taxResponse,
+                    'errors' => $this->errors,
+                );
+            }
+        } catch (\Exception $e) {
+            array_push($this->errors, $e->getMessage());
+            if (! $returnResponse) {
+                return false;
+            } else {
+                return array(
+                    'errors' => $this->errors,
+                );
             }
         }
     }
+
+}
--- src/TaxRequest/Address.php
+++ src/TaxRequest/Address.php
@@ -1,59 +1,93 @@
 <?php
-    namespace AccurateTax\TaxRequest;
+namespace AccurateTax\TaxRequest;
 
-    class Address {
-        private $address1;
-        private $address2;
-        private $city;
-        private $state;
-        private $zip;
-        private $plus4;
+class Address
+{
+    private $address1;
+    private $address2;
+    private $city;
+    private $state;
+    private $zip;
+    private $plus4;
+    private $country;
 
-        /**
-         * Create a new Address
-         *
-         * @param string $address1
-         * @param string $address2
-         * @param string $city
-         * @param string $state
-         * @param string $zip
-         * @param string $plus4
-         */
-        public function __construct($address1, $address2, $city, $state, $zip, $plus4='') {
-            $this->address1 = $address1;
-            $this->address2 = $address2;
-            $this->city = $city;
-            $this->state = $state;
-            $this->zip = $zip;
-            $this->plus4 = $plus4;
+    /**
+     * Create a new Address
+     *
+     * @param string $address1
+     * @param string $address2
+     * @param string $city
+     * @param string $state
+     * @param string $zip
+     * @param string $plus4
+     * @param string $country
+     */
+    public function __construct($address1, $address2, $city, $state, $zip, $plus4 = '', $country = 'US')
+    {
+        $this->address1 = $address1;
+        $this->address2 = $address2;
+        $this->city     = $city;
+        $this->state    = $state;
+        $this->zip      = $zip;
+        $this->plus4    = $plus4;
+
+        if (strtoupper($country) == 'USA' || strtoupper($country) == 'US' || strtoupper($country) == 'UNITED STATES') {
+            $country = 'US';
+        }
+        if (strtoupper($country) == 'CA' || strtoupper($country) == 'CAN' || strtoupper($country) == 'CANADA') {
+            $country = 'CA';
+        }
+        if (! in_array(strtoupper($country), ['US', 'CA'])) {
+            throw new \Exception('Invalid country. Only US and CA are accepted.');
         }
 
-        /**
-         * Get the State
-         *
-         * @return string
-         */
-        public function getState() {
-            return $this->state;
-        }
-
-        /**
-         * get the XML for the Address
-         *
-         * @return string
-         */
-        public function getXml($wrapper) {
-            $xml = '<' . $wrapper . '>';
-            $xml .= '<address_line1><![CDATA[' . $this->address1 .']]></address_line1>';
-            $xml .= '<address_line2>' . (!empty($this->address2) ? '<![CDATA[' : '' ) . $this->address2 . (!empty($this->address2) ? ']]>' : '' ) .'</address_line2>';
-            $xml .= '<city>' . $this->city .'</city>';
-            $xml .= '<state>' . $this->state .'</state>';
-            $xml .= '<zip>' . $this->zip .'</zip>';
-            if (!empty($this->plus4) && !is_null($this->plus4)) {
-                $xml .= '<plus4>' . $this->plus4 .'</plus4>';
-            }
-            $xml .= '</' . $wrapper . '>';
-
-            return $xml;
-        }
+        $this->country = $country;
     }
+
+    /**
+     * Get the State
+     *
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Get the Country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * get the XML for the Address
+     *
+     * @return string
+     */
+    public function getXml($wrapper)
+    {
+        $xml  = '<' . $wrapper . '>';
+        $xml .= '<address_line1><![CDATA[' . $this->address1 . ']]></address_line1>';
+        $xml .= '<address_line2>' . (! empty($this->address2) ? '<![CDATA[' : '') . $this->address2 . (! empty($this->address2) ? ']]>' : '') . '</address_line2>';
+        $xml .= '<city>' . $this->city . '</city>';
+        $xml .= '<state>' . $this->state . '</state>';
+        if ($this->country = 'US' && $wrapper == 'shipto') {
+            $xml .= '<zip>' . $this->zip . '</zip>';
+            if (! empty($this->plus4) && ! is_null($this->plus4)) {
+                $xml .= '<plus4>' . $this->plus4 . '</plus4>';
+            }
+        } else if ($this->country == 'CA' && $wrapper == 'shipto') {
+            $xml .= '<postalcode>' . $this->zip . '</postalcode>';
+            $xml .= '<country>' . $this->country . '</country>';
+        }
+        $xml .= '</' . $wrapper . '>';
+
+        return $xml;
+    }
+
+}
